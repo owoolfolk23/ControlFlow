8bb4ad899a0e034d5a8401966967204b37f66d63
owoolfolk23 <138935511+owoolfolk23@users.noreply.github.com>
2024-09-15 01:18:19 -0500

    added Part 2 of testing.

--- scripts/ControlFlow.js
+++ scripts/ControlFlow.js
@@ -1,10 +1,11 @@
 const pi = 3.1415;
-const radius = 5;
-const area = pi * radius * radius;
+let radius = 5;
+let area = pi * radius * radius;
 let weeks = 1;
 const space = 0.8;
 const plantRate = 2;
-let plantArea = space * 20 * (Math.pow(plantRate, weeks));
+let plantCount = 20
+let plantArea = space * plantCount * (Math.pow(plantRate, weeks));
 
 //* Part 1, Week 1 plant area testing *//
 console.group("Week 1")
@@ -59,3 +60,27 @@ else{
 console.groupEnd();
 
 //* Part 2 *//
+console.group("Part 2")
+plantCount = 100;
+let adjustedtesting = plantCount / 20;
+weeks = 10;
+radius = adjustedtesting * 5;
+area = adjustedtesting * (pi * radius * radius)
+console.log("The upscaled garden size required for starting with 100 plants is: "+area)
+plantArea = space * plantCount * (Math.pow(plantRate, weeks))
+if(plantArea > area * 0.8){
+    console.log("Pruned - exceeding the capacity of the garden")
+    console.log("plantArea is: "+plantArea)
+}
+else if(plantArea > area * 0.5){
+    console.log("Monitored - growing at an acceptable rate.")
+    console.log("plantArea is: "+plantArea)
+}
+else{
+    console.log("Planted - room to plant more plants")
+    console.log("plantArea is: "+plantArea)
+}
+console.log("The radius is: "+radius)
+console.groupEnd()
+//* Part 3 *//
+console.group("Part 3")
